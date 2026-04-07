8604c7ea2d9bb0b7cb512b2d39a98a66aa014a65
Miguel Aquiles <m.a27rombbl@gmail.com>
2026-04-07 16:46:11 -0400

    Ultima modifica~çao no Contador de While
    Why,it's so... so.

--- attv.js
+++ attv.js
@@ -2,16 +2,17 @@
 //para comentar várias linhas shift + alt + a.
 const r = require('readline-sync');
 
-//contador de 0 a 10, mas ele não termina até passar de 10. Ainda incompleto.
-/* let contador1 = 0;
+//contador de 0 a 10, mas ele não termina até passar de 9. Ainda incompleto.
+let contador1 = 0;
 
-while (contador1 <= 10) {
-    contador1 = parseFloat(r.question("Digite um Número de (0 a 10): "));
+while (contador1 <= 9) {
+    contador1 = parseFloat(r.question("Digite uma Nota de (0 a 10): "));
+   
     if (contador1 < 0 || contador1 > 10) {
-        console.log("Numero inválido. Digite um Número de (0 a 10): ");
+        console.log("Nota inválida.");
     }
     
-} */
+} console.log("Nota válida: " + contador1);
 
 //contador de 0 a 10 com Do While.
 /* let nota;
@@ -27,6 +28,7 @@ do {
 
 console.log("Nota válida: " + nota); */
 
+//???
 /* let soma = 0;
 let contador = 1;
 
@@ -48,7 +50,7 @@ let media = soma / 5;
 console.log("A média é: " + media.toFixed(2)); */
 
 //media com Do While.
-let somaDoWhile = 0;
+/* let somaDoWhile = 0;
 let contadorDoWhile = 1;
 
 do {
@@ -58,4 +60,4 @@ do {
 } while (contadorDoWhile <= 5);
 
 let mediaDoWhile = somaDoWhile / 5;
-console.log("A média é: " + mediaDoWhile.toFixed(2));
+console.log("A média é: " + mediaDoWhile.toFixed(2)); */
